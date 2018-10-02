621466bcc1583cd188397d4e53b468ba62d84dfe
Jonathan <32244961+llnwn@users.noreply.github.com>
2018-10-01 22:51:51 -0400

    LBPK Code Patch

--- www/api/v1/policy.view.xml.php
+++ www/api/v1/policy.view.xml.php
@@ -1,2 +1,2 @@
 <?php /* No code yet but .php is needed to allow POST method */?>
-<?xml version="1.0" encoding="UTF-8"?><result><status><id>0</id><message>Successful completion</message></status><response><policy name="Online User Agreement" id="21" is_accepted="true"></policy></response></result>
+<?xml version="1.0" encoding="UTF-8"?><result><status><id>0</id><message>Successful completion</message></status><response><policy name="Online User Agreement" id="21" is_accepted="false">[KARTING POLICY GOES HERE]</policy></response></result>
