a8917f29142306faf53729a863edfeed9ec957dc
Jonathan <32244961+llnwn@users.noreply.github.com>
2019-03-23 17:29:19 -0400

    Finished achievements.list.xml

--- www/api/v1/achievement.list.xml.php
+++ www/api/v1/achievement.list.xml.php
@@ -13,10 +13,23 @@
  * GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
- **/ 
+ **/
+declare(strict_types = 1);
 require_once(__DIR__ . '/../../index.php');
 use Karting\Skeleton\Request;
 use Karting\Skeleton\Response;
+use Karting\Utilities\Pagination;
+$page = 1; $per_page = 50; $limit = 50;
+if (!is_null(Request::GET_Number('page'))) {
+  $page = min(max(0, Request::GET_Number('page')), 1000);
+}
+if (!is_null(Request::GET_Number('per_page'))) {
+  $per_page = min(max(1, Request::GET_Number('per_page')), 50);
+}
+if (!is_null(Request::GET_Number('limit'))) {
+  $limit = min(max(0, Request::GET_Number('limit')), 1000);
+}
+$pagination = new Pagination($page, $per_page, $limit);
 $Response->startElement('result');
   $Response->startElement('status');
     $Response->writeElement('id', SUCCESSFUL_RESPONSE_CODE);
@@ -24,11 +37,11 @@ $Response->startElement('result');
   $Response->endElement();
   $Response->startElement('response');
     $Response->startElement('achievements');
-      $Response->writeAttribute('total', 50);
-      $Response->writeAttribute('row_start', 1);
-      $Response->writeAttribute('row_end', 50);
-      $Response->writeAttribute('page', 1);
-      $Response->writeAttribute('total_pages', 1);
+      $Response->writeAttribute('total', $pagination->limit);
+      $Response->writeAttribute('row_start', $pagination->accumulated);
+      $Response->writeAttribute('row_end', $pagination->stickied);
+      $Response->writeAttribute('page', $pagination->selected);
+      $Response->writeAttribute('total_pages', $pagination->pages);
     $Response->endElement();
   $Response->endElement();
 $Response->endElement();
